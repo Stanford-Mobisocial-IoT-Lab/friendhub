7d4a868d996902c677a403f9de68e5dd70fa76de
Michael Araya <maraya99@stanford.edu>
2018-05-14 12:16:04 -0700

    Update partnerfinder schema.

--- partner_finder/models.py
+++ partner_finder/models.py
@@ -6,7 +6,7 @@ class Activity(models.Model):
     organizer = models.CharField(max_length=50)
     activity = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    start_time = models.DateTimeField(auto_now=True)
-    end_time = models.DateTimeField(auto_now=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
     post_time = models.DateTimeField(auto_now=True) # when post was made
 
