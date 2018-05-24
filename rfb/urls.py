fefff65fd7d481423494b01460be3d8ab59f3951
Matthew Millican <millimat@stanford.edu>
2018-05-24 10:43:20 -0700

    Removed duplicate paths in rfb/urls.py

--- rfb/urls.py
+++ rfb/urls.py
@@ -18,12 +18,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-	path(r'admin/', admin.site.urls),
-	path(r'api/bbs/', include('bbs.urls')),
-	path(r'api/sticky_notes/', include('sticky_notes.urls')),
-	path(r'api/polls/', include('polls.urls')),
     path(r'admin/', admin.site.urls),
     path(r'api/bbs/', include('bbs.urls')),
     path(r'api/sticky_notes/', include('sticky_notes.urls')),
+    path(r'api/polls/', include('polls.urls')),
     path(r'api/partner_finder/', include('partner_finder.urls'))
 ]
