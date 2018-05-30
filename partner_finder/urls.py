c6a0eecdb2f291e6ba3adeab5e5f8374431187f9
Silei Xu <silei@cs.stanford.edu>
2018-05-30 16:42:56 -0700

    add a naive way to compute matches

--- partner_finder/urls.py
+++ partner_finder/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'activities/', views.ActivityList.as_view())
+    path(r'activities/', views.ActivityList.as_view()),
+    path(r'matches/', views.MatchList.as_view())
 ]
--- partner_finder/views.py
+++ partner_finder/views.py
@@ -23,3 +23,28 @@ class ActivityList(APIView):
             serializer.save()
             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MatchList(APIView):
+    permission_classes = (permissions.IsAuthenticated, )
+    authentication_classes = (BasicAuthentication, )
+
+    @staticmethod
+    def get(request):
+        activities = Activity.objects.all()
+        matches = []
+        for a1 in activities:
+            for a2 in activities:
+                if a1.email != a2.email and a1.activity == a2.activity:
+                    max_start_time = max(a1.start_time, a2.start_time)
+                    min_end_time = max(a1.end_time, a2.end_time)
+                    if max_start_time < min_end_time:
+                        matches.append({
+                            'email': a1.email,
+                            'match_organizer': a2.organizer,
+                            'match_email': a2.email,
+                            'start_time': max_start_time,
+                            'end_time': min_end_time
+                        })
+        return JsonResponse(matches, safe=False)
+
